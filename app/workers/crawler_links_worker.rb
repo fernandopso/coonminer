df4b91057c8672d5bb2aaa14505717843cacae57
Fernando <fernandopso@bsi.ufla.br>
2019-02-17 20:42:05 -0300

    Perform crawler of links.title as async mode
    To to improve performance

--- app/workers/crawler_links_worker.rb
+++ app/workers/crawler_links_worker.rb
@@ -1,62 +1,17 @@
-require 'open-uri'
-require 'open_uri_redirections'
+# frozen_string_literal: true
 
 class CrawlerLinksWorker
   include Sidekiq::Worker
 
-  LOG_NAME = 'CrawlerLinksWorker >'
-
-  CHAR_CODE = {
-    "&#233;" => "é",
-    "&#231;" => "ç",
-    "&#227;" => "õ",
-    "&quot;" => "",
-    "&laquo;" => "",
-    "&nbsp;" => " ",
-    "&rsquo;" => "",
-    "&raquo;" => "",
-    "&#8211;" => "-",
-    "&#39;" => "",
-    "&eacute;" => "é",
-    "&Eacute;" => "É",
-    "&egrave;" => "è",
-    "&#039;" => ""
-  }
-
   def perform
-    logger.info "#{LOG_NAME} start"
+    Category.first.tokens.each do |token|
+      token.links.order_by_df_desc.take(3).each do |link|
+        next unless link.href
 
-    token = Token.collect_at_desc.sample
+        logger.info "Perform async to url #{link.href} of token #{token.uuid}"
 
-    logger.info "#{LOG_NAME} take token #{token.word}"
-
-    links = token.links.where(title: nil).order(df: :desc).take(10)
-
-    links.each do |link|
-      begin
-        html = open(link.href, allow_redirections: :all).read
-
-        begin_title = html =~ /<title>/
-        final_title = html =~ /<\/title>/
-
-        title = replace_char(html[7 + begin_title..final_title - 1].encode("UTF-8"))
-
-        link.update title: title
-
-        logger.info "#{LOG_NAME} #{link.href} #{link.title}"
-      rescue => exception
-        logger.info "#{LOG_NAME} Error: #{exception}"
+        Links::TitleWorker.perform_async(link.href, link.id)
       end
     end
-
-    logger.info "#{LOG_NAME} token to token #{token.word}"
-  end
-
-  private
-
-  def replace_char(title)
-    CHAR_CODE.keys.each { |key| title.gsub!(key, CHAR_CODE[key]) }
-
-    title
   end
 end
---
+++ app/workers/links/title_worker.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'open_uri_redirections'
+
+module Links
+  class TitleWorker
+    include Sidekiq::Worker
+
+    sidekiq_options retry: 0
+
+    def perform(url, link_id)
+      Link.find(link_id).update title: title_for(request_page(url))
+    end
+
+    private
+
+    def request_page(url)
+      open(url, allow_redirections: :all).read
+    end
+
+    def title_for(html)
+      html[7 + begin_title(html)..final_title(html) - 1].encode('UTF-8')
+    end
+
+    def begin_title(body)
+      body =~ /<title>/
+    end
+
+    def final_title(body)
+      body =~ /<\/title>/
+    end
+  end
+end
