d2bed0c5803acafe435677d5943d4905b152350c
fernandopso <fernandopso3@gmail.com>
2020-05-08 11:55:19 -0300

    update gems

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+if Rails.env.production? && ENV['COONMINER_SENTRY_DSN']
   Raven.configure do |config|
     config.dsn = ENV['COONMINER_SENTRY_DSN']
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
